9a281239d132ea4954907200d24988e0b057ed3a
shaileshpandit141 <shaileshpandit141@gmail.com>
2023-12-12 20:12:04 +0530

    Add Page Loader

--- Roadmaps/DSA-With-Python-Syllabus/src/App.js
+++ Roadmaps/DSA-With-Python-Syllabus/src/App.js
@@ -14,9 +14,26 @@ function renderToDocument() {
         data.subHeadings.forEach(subHed => {
             liEl.insertAdjacentHTML('beforeend', `<li>${subHed}</li>`)
         })
-        
+
     });
 
 }
 
 renderToDocument()
+
+
+// || Hendel Loder Element
+const loderEl = document.getElementById('loder')
+window.addEventListener('load', event => {
+    event.preventDefault()
+
+    // || Adding css Class to hide loader
+    loderEl.classList.add('loder--hidden')
+
+    loderEl.addEventListener('transitionend', event => {
+        event.preventDefault()
+
+        // || For Deleting element from the document 
+        document.body.removeChild(loderEl)
+    })
+})
--- src/App.js
+++ src/App.js
@@ -33,3 +33,20 @@ searchBar.addEventListener('input', event => {
         // roadmapCardsWrapper.insertAdjacentHTML('afterbegin', "<h4 class='no--metch'>Oop's! No metch for it.</>")
     }
 })
+
+
+// || Hendel Loder Element
+const loderEl = document.getElementById('loder')
+window.addEventListener('load', event => {
+    event.preventDefault()
+
+    // || Adding css Class to hide loader
+    loderEl.classList.add('loder--hidden')
+
+    loderEl.addEventListener('transitionend', event => {
+        event.preventDefault()
+
+        // || For Deleting element from the document 
+        document.body.removeChild(loderEl)
+    })
+})
